504b362c6b07add815b1b9afcb2df4f27fb0dfd7
Gaurav Jasiwal <jasiwalgaurav@gmail.com>
2022-08-29 00:43:11 +0530

    Display live feed or manual feed

--- core/src/main/java/com/aem/fedex/core/models/RSSFeedModel.java
+++ core/src/main/java/com/aem/fedex/core/models/RSSFeedModel.java
@@ -15,59 +15,114 @@
  */
 package com.aem.fedex.core.models;
 
+import com.day.cq.wcm.api.Page;
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.ParsingFeedException;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.settings.SlingSettingsService;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.jdom.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
-
-@Model(adaptables = Resource.class)
+@Model(
+        adaptables = {SlingHttpServletRequest.class, Resource.class},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "fedex/components/rssfeed")
 public class RSSFeedModel {
 
-    @ValueMapValue(name = PROPERTY_RESOURCE_TYPE, injectionStrategy = InjectionStrategy.OPTIONAL)
-    @Default(values = "No resourceType")
-    protected String resourceType;
-
-    @OSGiService
-    private SlingSettingsService settings;
-    @SlingObject
-    private Resource currentResource;
-    @SlingObject
-    private ResourceResolver resourceResolver;
-
-    private String message;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RSSFeedModel.class);
+    @ChildResource(name = "rssFeeds")
+    Collection<RSSFeedModel.RSSFeed> rssFeeds;
+    @Inject
+    private Page currentPage;
+    @Self
+    private SlingHttpServletRequest request;
+    @Inject
+    @Via(value = "resource")
+    private int noOfItems;
 
     @PostConstruct
     protected void init() {
-
+        List<RSSFeedModel.RSSFeed> liveEnteries = new ArrayList<>();
         try {
             URL feedSource = new URL("https://sports.ndtv.com/rss/cricket");
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(feedSource));
-            message = feed.getLink();
-            //feed.getEntries().forEach();
+
+            feed.getEntries().subList(0, noOfItems).forEach(eachFeed ->{
+                RSSFeedModel.RSSFeed eachLiveFeed = new RSSFeedModel.RSSFeed();
+                SyndEntry syndFeed = (SyndEntry) eachFeed;
+                eachLiveFeed.setTitle(syndFeed.getTitle());
+                eachLiveFeed.setDescription(syndFeed.getDescription().getValue());
+                eachLiveFeed.setPublishDate(((ArrayList<Element>)syndFeed.getForeignMarkup()).get(0).getContent().get(0).toString());
+                liveEnteries.add(eachLiveFeed);
+            });
+
+            if (!liveEnteries.isEmpty()){
+                rssFeeds = liveEnteries;
+            }
+
+
         } catch (IOException | FeedException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("Error while parsing the RSS feed", e);
         }
 
     }
 
-    public String getMessage() {
-        return message;
+
+
+    public Collection<RSSFeedModel.RSSFeed> getRssFeeds() {
+        return rssFeeds;
     }
 
+    @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+    public static class RSSFeed {
+        @Inject
+        private String title;
+        @Inject
+        private String description;
+        @Inject
+        private String publishDate;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getPublishDate() {
+            return publishDate;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public void setPublishDate(String publishDate) {
+            this.publishDate = publishDate;
+        }
+    }
 }
